da5209a4c4ec7a655e505a26ecb52f78cea0c5be
lkovanen <lkovanen>
2009-05-25 11:58:37 +0000

    Style fixes.

--- misc/com_struct_topol_analysis.py
+++ misc/com_struct_topol_analysis.py
@@ -1,10 +1,13 @@
 """
-This script takes two input files (the full net, and the communities) and one output directory as
-arguments, and gives out a file with average values of different properties
-of communities as a function of com. size in the following format:
-N_NODES N_COMMUNITIES EDGE_DENSITY EDGE_WEIGHT INTENSITY COHERENCE SHORTEST_PATH_LENGTH DIAMETER
+This script takes two input files (the full net, and the communities)
+and one output directory as arguments, and gives out a file with
+average values of different properties of communities as a function of
+com. size in the following format:
+N_NODES N_COMMUNITIES EDGE_DENSITY EDGE_WEIGHT INTENSITY COHERENCE
+SHORTEST_PATH_LENGTH DIAMETER
 
-To get a description of the input arguments on screen, run the script without args or see below:
+To get a description of the input arguments on screen, run the script
+without args or see below:
 
 python com_struct_topol_analysis.py
 
@@ -17,15 +20,19 @@ Output file name is on the format:
 {full net's edge file name}_average_community_analysis.txt
 
 EXAMPLE OF USE:
-python com_struct_topol_analysis.py 4-3_cliques_links_nonunit_w.edg 4-3_cliques_links.txt_cliques_3_0_0_communities_0 /home/eiesland/networks/Jon/communities/python_code/4-3_testdir/
+python com_struct_topol_analysis.py 4-3_cliques_links_nonunit_w.edg
+   4-3_cliques_links.txt_cliques_3_0_0_communities_0
+   /home/eiesland/networks/Jon/communities/python_code/4-3_testdir/
 
 Jon Eiesland
 May 2009
 """
 
-from netpython import * # lce-made net library found in networks/EDEN/nwtool/netpython
+from netpython import * # lce-made net library found in
+                        # networks/EDEN/nwtool/netpython
+
 from sys import argv
-from numpy import zeros
+import numpy as np
 
 if len(argv) < 4:
     print "Please give arguments. \n \
@@ -34,36 +41,36 @@ if len(argv) < 4:
     3 - directory for analysis output (ending with '/') \n "
     exit()
 
-#Args
+# Args
 full_net_edg = argv[1]
 comp_nodes_list = argv[2]
 out_dir = argv[3]
 
-#Open full net, arg 1
+# Open full net, arg 1
 print 'Beginning to read full net (Phone net, N_nodes 10^6 took 8 mins) ...'
 net=netio.loadNet(full_net_edg)
 print 'Full net read. Reading communities, doing analysis and writing to file...'
 
-#Read component nodes list, arg 2
+# Read component nodes list, arg 2
 f=open(comp_nodes_list)
 communities=f.readlines()
 f.close()
 
-#make: n_nodes <> n_comommunities from arg 2
+# make: n_nodes <> n_comommunities from arg 2
 n_nodessort = [] #temp. containers
 n_comsort = []
 for line in communities:
-    com_nodes = line.split()
+    com_nodes = map(int, line.split())
     N = len(com_nodes)
-    com_nodes=map(lambda x: int(x), com_nodes)
-#insert into corresponding indices
+    # Insert into corresponding indices
     if (N not in n_nodessort):
         n_nodessort.append(N)
         n_comsort.append(1)
     else:
         n_comsort[n_nodessort.index(N)] += 1
-#sort and update
-n_nodes= [] #final containers
+
+# Sort and update
+n_nodes= [] # Final containers
 n_com = []
 for e in n_nodessort:
     n_nodes.append(e)
@@ -81,39 +88,38 @@ for es in n_nodes:
 
 #for each community, get the subnet and do the analysis
 cont_length = len(n_nodes) # number of different com. sizes
-cont_rho = zeros(cont_length) #containers of the different averages for the com. sizes
-cont_w = zeros(cont_length)
-cont_q = zeros(cont_length)
-cont_I = zeros(cont_length)
-cont_pl = zeros(cont_length)
-cont_d = zeros(cont_length)
+cont_rho = np.zeros(cont_length, float) #containers of the different averages for the com. sizes
+cont_w = np.zeros(cont_length, float)
+cont_q = np.zeros(cont_length, float)
+cont_I = np.zeros(cont_length, float)
+cont_pl = np.zeros(cont_length, float)
+cont_d = np.zeros(cont_length, float)
 
 for line in communities:
-    com_nodes=line.split() #list of the nodes in this community
+    com_nodes = map(int, line.split()) #list of the nodes in this community
     N = len(com_nodes)     #number of nodes in com                
-    com_nodes=map(lambda x: int(x), com_nodes)
-    com_net=netext.getSubnet(net, com_nodes) #get an instance of type 'net' of the community
+    com_net = netext.getSubnet(net, com_nodes) #get an instance of type 'net' of the community
     com_net_edges = com_net.edges #get the edges inside the community
     E = len(com_net_edges) #number of edges in com
     index = n_nodes.index(N) #get the index in container to input averages for this com. also used to get the number of communities with N nodes from n_com[index]
 
-#edge density
+    # Edge density
     rho = 2. * E / N / (N-1)
     cont_rho[index] += rho / n_com[index]
 
-#edge weight
+    # Edge weight
     w = 0
     for e in com_net_edges:
         w += e[2] #source dest weight
     cont_w[index] += w / E / n_com[index]
 
-#intensity
+    # Intensity
     I = 1
     for e in com_net_edges:
         I *= pow(e[2], 1./E)
     cont_I[index] += I / n_com[index]
 
-#shortest path length and diameter
+    # Shortest path length and diameter
     node_shortest_pl = [] #container for shortest path length for each node
     diameter = 0.
     for e in com_nodes: #do for each node in community
@@ -127,10 +133,9 @@ for line in communities:
     cont_pl[index] += av_sh_pl / n_com[index]
     cont_d[index] += float(diameter) / n_com[index]
 
-#coherence
+# Coherence
 for i in range(cont_length):
     cont_q[i] += cont_I[i] / cont_w[i]
-
 
 #Print to file
 out_file_name = full_net_edg.split('/')[-1]
@@ -138,18 +143,17 @@ out_file_name = out_file_name.split('.')[0]
 all_outfile = out_dir + out_file_name + '_average_community_analysis.txt'
 o = open(all_outfile, 'w')
 #header
-o.write('N_NODES' + "\t" + 'N_COMMUNITIES' + "\t" +
-            'EDGE_DENSITY'.rjust(14) + "\t" + 'EDGE_WEIGHT'.rjust(14)
-            + "\t" + 'INTENSITY'.rjust(14) + "\t" +
-            'COHERENCE'.rjust(14) + "\t" + 'SHORTEST_PL'.rjust(14) +
-            "\t" + 'DIAMETER' + "\n")
+headers = ('N_NODES', 'N_COMMUNITIES', 'EDGE_DENSITY'.rjust(14),
+           'EDGE_WEIGHT'.rjust(14), 'INTENSITY'.rjust(14),
+           'COHERENCE'.rjust(14), 'SHORTEST_PL'.rjust(14), 'DIAMETER')
+o.write("t".join(headers) + "\n")
 
 for i in range(cont_length):
-    o.write(str(n_nodes[i]).rjust(7) + "\t" +
-    str(n_com[i]).rjust(13) + "\t" + str(cont_rho[i]).rjust(14) + "\t"
-    + str(cont_w[i]).rjust(14) + "\t" + str(cont_I[i]).rjust(14) +
-    "\t" + str(cont_q[i]).rjust(14) + "\t" + str(cont_pl[i]).rjust(14)
-    + "\t" + str(cont_d[i]).rjust(8) + "\n")
+    data = (str(n_nodes[i]).rjust(7), str(n_com[i]).rjust(13),
+            str(cont_rho[i]).rjust(14), str(cont_w[i]).rjust(14),
+            str(cont_I[i]).rjust(14), str(cont_q[i]).rjust(14),
+            str(cont_pl[i]).rjust(14), str(cont_d[i]).rjust(8))
+    o.write("\t".join(data) + "\n")
 
 
 #For getting what's printed to file on screen, uncomment ->
