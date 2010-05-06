9dc6ae7e8194a2a75a4851893cabb54c5ae2490e
lkovanen <lkovanen>
2010-05-06 14:41:09 +0000

    savefig now can do svg, and get_rcParams allows setting a default font size.

--- misc/fig_utils.py
+++ misc/fig_utils.py
@@ -1,22 +1,44 @@
 # Different command for making figures.
 
+import sys
 import os
 import pylab
 import matplotlib.ticker as ticker
 import numpy as np
 
-def savefig(fig, name, extensions, verbose=False):
+def savefig(fig, name, extensions=None, verbose=False):
     """Save figure.
 
     Save matplotlib.figure object `fig` as `name`.EXT, where EXT are
-    given in `extensions`.
+    given in `extensions`. If only one save type is used, the full
+    name (including the extension) can also be given as `name`.
+
+    Note! Saving as 'svg' requires that the program 'pdf2svg' is
+    installed on your system.
     """
+
     if len(name) == 0:
         raise ValueError("File name can not be empty.")
+
+    if extensions is None:
+        fields = name.split(".")
+        if len(fields) == 1:
+            raise ValueError("File name must contain an extension if"
+                             " extensions are not given explicitely.")
+        extensions = fields[-1]
+        name = ".".join(fields[:-1])
 
     if isinstance(extensions, str):
         extensions = (extensions,)
     
+    # Check if pdf should be generated (both eps and svg will be
+    # created from pdf) and generate if necessary.
+    pdf_generated = False
+    pdf_tmp = "%s_tmp_%d.pdf" % (name, np.random.randint(100000))
+    if set(['pdf','svg','eps']).intersection(extensions):
+        fig.savefig(pdf_tmp)
+        pdf_generated = True
+
     for ext in extensions:
         if not isinstance(ext, str):
             raise ValueError("'extensions' must be a list of strings.")
@@ -24,15 +46,32 @@ def savefig(fig, name, extensions, verbose=False):
             ext = ext[1:]
             
         if ext == 'eps':
-            # Save as pdf, convert to eps and remove the original pdf.
-            fig.savefig(name + ".pdf")
-            os.popen("pdftops -eps %s" % (name + ".pdf",))
-            if 'pdf' not in extensions:
-                os.popen("rm %s" % (name + ".pdf",))
-        else:
+            pipe = os.popen("pdftops -eps %s %s.eps" % (pdf_tmp, name))
+            exit_status = pipe.close()
+            if exit_status:
+                if os.WEXITSTATUS(exit_status) == 127:
+                    sys.stderr.write("%s could not be created because program "
+                                     "'pdftoeps' could not be found.\n" % (ext,))
+                else:
+                    sys.stderr.write("Problem saving '%s'.\n" % (ext,))
+        elif ext == 'svg':
+            pipe = os.popen("pdf2svg %s %s.svg" % (pdf_tmp, name))
+            exit_status = pipe.close()
+            if exit_status:
+                if os.WEXITSTATUS(exit_status) == 127:
+                    sys.stderr.write("%s could not be created because program "
+                                     "'pdf2svg' could not be found.\n" % (ext,))
+                else:
+                    sys.stderr.write("Problem saving '%s'.\n" % (ext,))
+        elif ext != 'pdf':
             # fig.savefig raises a ValueError if the extension is not identified.
             fig.savefig(name + "." + ext)
 
+    if pdf_generated:
+        if 'pdf' in extensions:
+            os.popen("mv %s %s.pdf" % (pdf_tmp,name))
+        else:
+            os.popen("rm %s" % (pdf_tmp,))
 
 def get_rcParams(fig_width_cm, fig_ratio = 0.8, font_sizes = None):
     """Set good parameters for LaTeX-figures.
@@ -49,16 +88,17 @@ def get_rcParams(fig_width_cm, fig_ratio = 0.8, font_sizes = None):
         The ratio height/width. < 1 is landscape, 1.0 is square and
         > 1.0 is portrait.
     font_sizes: dictionary
-        The sizes of the fonts used in the figure. Default is size 8
-        for everything else expect 10 for the title. Possible keys are
-        'label', 'title', 'text', 'legend', 'tick', the meaning should
-        be self explanatory.
+        The font sizes used in the figure. Default is size 8 for
+        everything else expect 10 for the title. Possible keys are
+        'default', 'label', 'title', 'text', 'legend' and 'tick'.
+        'default' is used when the specific value is not defined,
+        other keys should be self explanatory.
     """
     default_font_sizes = {'label':8, 'title':10, 'text':8, 'legend':8, 'tick':8}
     font_sizes = (font_sizes or {})
     for k in default_font_sizes:
         if k not in font_sizes:
-            font_sizes[k] = default_font_sizes[k]
+            font_sizes[k] = (font_sizes.get('default') or default_font_sizes[k])
 
     inches_per_cm = 1/2.54
     fig_width = 1.0*fig_width_cm*inches_per_cm  # width in inches
@@ -69,6 +109,7 @@ def get_rcParams(fig_width_cm, fig_ratio = 0.8, font_sizes = None):
               'axes.labelsize': font_sizes['label'],
               'axes.titlesize': font_sizes['title'],
               'text.fontsize': font_sizes['text'],
+              'font.size': font_sizes['text'],
               'legend.fontsize': font_sizes['legend'],
               'xtick.labelsize': font_sizes['tick'],
               'ytick.labelsize': font_sizes['tick'],
