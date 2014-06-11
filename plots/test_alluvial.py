deefbe45a00bf0112dba6266570b6d02939fca75
Richard Darst <rkd@zgib.net>
2014-06-11 16:19:49 +0300

    Wrap test_alluvial script in in test() function for importability.

--- plots/test_alluvial.py
+++ plots/test_alluvial.py
@@ -1,58 +1,60 @@
 """ A module for testing/demonstrating the use of the alluvial.py module """
 import os
 
-import matplotlib as mpl
-mpl.use('Agg')
-from matplotlib import pyplot as plt
 from alluvial import plot_alluvial
 
 
-ribbon_size_matrix = [
-    [5, 2, 1],
-    [3, 4, 1],
-    [1, 0, 3]
-]
+def test():
+    import matplotlib as mpl
+    mpl.use('Agg')
+    from matplotlib import pyplot as plt
 
-ribbon_label_matrix = [
-    ["a", "b", "c"],
-    ["short", "medium", "long"],
-    ["3-1", "3-2", "3-3"]
-]
+    ribbon_size_matrix = [
+        [5, 2, 1],
+        [3, 4, 1],
+        [1, 0, 3]
+    ]
 
-stable_ribbon_sizes_1 = [
-    [2, 1, 1],
-    [3, 4, 1],
-    [1, 0, 2]
-]
+    ribbon_label_matrix = [
+        ["a", "b", "c"],
+        ["short", "medium", "long"],
+        ["3-1", "3-2", "3-3"]
+    ]
 
-stable_ribbon_sizes_2 = [
-    [5, 2, 1],
-    [3, 2, 1],
-    [1, 0, 3]
-]
+    stable_ribbon_sizes_1 = [
+        [2, 1, 1],
+        [3, 4, 1],
+        [1, 0, 2]
+    ]
 
-
-module_colors_1 = ["red", "green", "blue"]
-module_colors_2 = ["green", "red", "blue"]
-ribbon_bglim = 1.5
-
-fig = plt.figure()
-ax1 = fig.add_subplot(211)
-ax2 = fig.add_subplot(212)
+    stable_ribbon_sizes_2 = [
+        [5, 2, 1],
+        [3, 2, 1],
+        [1, 0, 3]
+    ]
 
 
-# the current full power:
-plot_alluvial(
-    ax1, ribbon_size_matrix, ribbon_label_matrix, module_colors_1,
-    module_colors_2, ribbon_bglim, stable_ribbon_sizes_1,
-    stable_ribbon_sizes_2, ribbon_label_size=10, ribbon_label_HA="center"
-)
+    module_colors_1 = ["red", "green", "blue"]
+    module_colors_2 = ["green", "red", "blue"]
+    ribbon_bglim = 1.5
 
-#  minimal setup
-plot_alluvial(ax2, ribbon_size_matrix)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
 
-fname =  os.path.join(os.path.dirname(__file__),
-                      "gallery/alluvial_example.pdf")
 
-#save fig to gallery
-plt.savefig(fname, format='pdf')
+    # the current full power:
+    plot_alluvial(
+        ax1, ribbon_size_matrix, ribbon_label_matrix, module_colors_1,
+        module_colors_2, ribbon_bglim, stable_ribbon_sizes_1,
+        stable_ribbon_sizes_2, ribbon_label_size=10, ribbon_label_HA="center"
+    )
+
+    #  minimal setup
+    plot_alluvial(ax2, ribbon_size_matrix)
+
+    fname =  os.path.join(os.path.dirname(__file__),
+                          "gallery/alluvial_example.pdf")
+
+    #save fig to gallery
+    plt.savefig(fname, format='pdf')
