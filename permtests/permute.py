5de7c2c69e82381b0ba2d97782847dfdcf28b6fd
Rainer Kujala <Rainer.Kujala@aalto.fi>
2014-06-17 11:06:57 +0300

    Finished testing functions for permutating stuff, the actual permutation tests are not yet tested

--- permtests/permute.py
+++ permtests/permute.py
@@ -26,19 +26,21 @@ def get_random_state(seed=None):
         return RandomState()
 
 
-def get_permutation(paired, n1, n2, rng, i):
+def get_permutation(paired, n1, n2, rng=None, i=None):
     """
     Helper function to compute a permutation with all the switches.
 
     Args:
-        paired: True or False
+        paired: True or False, whether to return a paired permutation
         n1: number of instances in the first group
         n2: number of instances in the second group
         rng: A random number generator (None for deterministic permutations)
         i: The "index" of the permutation, when permSamples = 'all' is used
     """
     if paired is True:
+        assert n1 == n2
         if rng is None:  # go through full permutation dist.
+            assert i is not None
             return _get_paired_permutation(n1, i)
         else:  # a paired setting but a random permutation
             return _get_random_paired_permutation(n1+n2, rng)
@@ -56,6 +58,15 @@ def _get_random_paired_permutation(nTot, rng):
 
 
 def _get_paired_permutation(n, i):
+    """Get a paired permutation corresponding to the index i
+    Args:
+        i: index of the permutation (in range 0<= i < 2**n)
+        n: number of *pairs*
+            (half of the total number of elements to be permuted)
+    """
+
+    assert i >= 0
+    assert i < 2**n
     permBase = np.zeros(n, dtype=np.int32)
     #compute the bin repr.
     for j in np.linspace(n-1, 0, n).astype(np.int32):
@@ -73,11 +84,23 @@ def permute_array(dataArray, perm):
 
 
 def permute_matrix(matrix, perm):
+    """
+    Permute the rows and columns of the (similarity) matrix.
+    (Destroys any 'grouping' effects)
+    """
+    assert matrix.shape[0] == matrix.shape[1], "should be a square matrix"
     return matrix[perm, :][:, perm]
 
 
-def half_permute_matrix(matrix, perm):
-    """ Got a full permutation, use only half of the permutation"""
+def half_permute_paired_matrix(matrix, perm):
+    """
+    Got a full permutation, use only half of the permutation.
+
+    This permutation destroys the paired structure of the matrix.
+    Keeps the upper left corner of the matrix untouched.
+    """
+    assert len(matrix) % 2 == 0, "matrix rank should be paired"
+    assert matrix.shape[0] == matrix.shape[1], "should be a square matrix"
     perm = np.array(perm)
     n1 = np.shape(matrix)[0]/2
     perm = perm[perm >= n1]
--- permtests/test_permute.py
+++ permtests/test_permute.py
@@ -26,8 +26,9 @@ class TestPermutation(unittest.TestCase):
                                 [31, 32, 33, 34],
                                 [41, 42, 43, 44]
                                 ])
-        self.mat_perm_1 = [3, 1, 2, 0]
-        self.mat_perm_2 = [1, 2, 3, 0]
+        self.mat_perm_1 = np.array([3, 1, 2, 0])
+        self.mat_perm_2 = np.array([1, 2, 3, 0])
+        self.rng = permute.get_random_state(seed=123456)
 
     def test_permute_array(self):
         newdata1 = permute.permute_array(self.data, self.perm1)
@@ -59,13 +60,92 @@ class TestPermutation(unittest.TestCase):
 
     def test_permute_matrix(self):
         perm_mat = permute.permute_matrix(self.matrix, self.mat_perm_1)
-        should_be = np.matrix([
-                              [44, 42, 43, 41],
-                              [24, 22, 23, 21],
-                              [34, 32, 33, 31],
-                              [14, 12, 13, 11]
-                              ])
-        self.as_arr_alm_eq(perm_mat, should_be)
+        result_should_be = np.matrix([
+            [44, 42, 43, 41],
+            [24, 22, 23, 21],
+            [34, 32, 33, 31],
+            [14, 12, 13, 11]]
+        )
+        self.as_arr_alm_eq(perm_mat, result_should_be)
+
+        perm_mat_2 = permute.permute_matrix(self.matrix, self.mat_perm_2)
+        result_should_be = np.matrix([
+            [22, 23, 24, 21],
+            [32, 33, 34, 31],
+            [42, 43, 44, 41],
+            [12, 13, 14, 11]]
+        )
+        self.as_arr_alm_eq(perm_mat_2, result_should_be)
+
+    def test_half_permute_paired_matrix(self):
+
+        initmat = np.matrix([
+            [100, 1, 10, 0],
+            [1, 100, 0, 10],
+            [10, 0, 900, 2],
+            [0, 10, 2, 800]
+        ])
+        half_perm_mat = permute.half_permute_paired_matrix(initmat,
+                                                           self.mat_perm_1
+                                                           )
+        result_should_be = np.matrix([
+            [100, 1, 0, 10],
+            [1, 100, 10, 0],
+            [0, 10, 800, 2],
+            [10, 0, 2, 900]
+        ])
+        self.as_arr_alm_eq(half_perm_mat, result_should_be)
+        result_should_be = \
+            initmat[np.array([0, 1, 3, 2]), :][:, np.array([0, 1, 3, 2])]
+        self.as_arr_alm_eq(half_perm_mat, result_should_be)
+
+    def assert_paired_permutation_validity(self, perm, n1):
+        """
+        Test that the permutation is a *paired* permutation
+        """
+        index_sums = []
+        for i in range(n1):
+            index_sums.append(perm[i]+perm[i+n1])
+        # one requirement for a paired permutation:
+        for i in range(n1-1):
+            assert index_sums[i] == index_sums[i+1] - 2
+        # more explicit requirement:
+        for i in range(2*n1):
+            pair = perm[i]
+            assert perm[pair] == i
+        self.assert_permutation_validity(perm, 2*n1)
+
+    def assert_permutation_validity(self, perm, n):
+        self.as_arr_alm_eq(np.sort(np.unique(perm)), np.arange(n))
+
+    def test_get_random_paired_permutation(self):
+        paired = True
+        n1 = n2 = 4
+        #to test for different permutations
+        for _ in range(10):
+            perm = permute.get_permutation(paired, n1, n2, self.rng)
+            self.assert_paired_permutation_validity(perm, n1)
+
+    def test_get_paired_permutation(self):
+        paired = True
+        perms = []
+        n1 = n2 = 4
+        for i in range(2**n1):
+            perm = permute.get_permutation(paired, n1, n2, None, i)
+            self.assert_paired_permutation_validity(perm, n1)
+            for other_perm in perms:
+                assert not (other_perm == perm).all()
+            perms.append(perm)
+
+    def test_get_normal_permutation(self):
+        paired = False
+        n1 = n2 = 4
+        for i in range(10):
+            p = permute.get_permutation(paired, n1, n2, rng=self.rng)
+            self.assert_permutation_validity(p, n1+n2)
+
+
+
 
 
 
